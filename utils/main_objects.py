0f12b9aff0575857add296840ccb5bb378b2bbd0
laurendudu <lauren.durivault@gmail.com>
2023-03-03 23:43:31 +0100

    Optimization process

---
+++ utils/main_objects.py
@@ -0,0 +1,71 @@
+import math
+
+
+def get_closest_rsu(rsu_list, object):
+    min_distance = math.inf
+    min_rsu = None
+
+    for rsu in rsu_list:
+        distance = math.sqrt((rsu.X - object.X) ** 2 + (rsu.Y - object.Y) ** 2)
+        if distance < min_distance and distance != 0:
+            min_distance = distance
+            min_rsu = rsu
+
+    return min_rsu
+
+
+def is_server_free(rsu):
+    return rsu.STATE == "IDLE"
+
+
+def compatible(rsu, task):
+    if rsu.ES == "AP" and task.TYPE == "DATA TRANSFER":
+        return True
+    elif rsu.ES != "AP" and task.TYPE == "COMPUTATION":
+        return True
+    else:
+        return False
+
+
+def computation_time(task_length: int, vm_nb: int, vm_cpu: int) -> float:
+    """Computes the computation time of a task.
+
+    Args:
+        task_length (int): length of the task in MI
+        vm_nb (int): number of VMs in the ES.
+        vm_cpu (int): CPU capacity of a VM in MIPS.
+
+    """
+    return task_length / (vm_nb * vm_cpu)
+
+
+def migration_time(file_size: int, distance: float, dtr: int) -> float:
+    """Computes the migration of a task between a AP RSU and an ES.
+
+    Args:
+        file_size (int): file size of the task in megabytes.
+        distance (float): distance between the RSUs in meters.
+        dtr (int): data transfer rate in megabits per second.
+
+    Returns:
+        float: migration of the task in seconds.
+    """
+
+    return (file_size / dtr) * distance
+
+
+def data_transfer_time(file_size: int, dtr: int) -> float:
+    """Computes the data transfer time of a task.
+
+    Args:
+        file_size (int): file size of the task in megabytes.
+        dtr (int): data transfer rate in megabits per second.
+
+    Returns:
+        float: data transfer time of the task in seconds.
+    """
+    return file_size / dtr
+
+
+def distance(object_1, object_2):
+    return math.sqrt((object_1.X - object_2.X) ** 2 + (object_1.Y - object_2.Y) ** 2)
---
+++ utils/objects.py
@@ -0,0 +1,141 @@
+import pandas as pd
+import numpy as np
+
+
+class RSU:
+    def __init__(
+        self,
+        ID,
+        X,
+        Y,
+        DTR,
+    ):
+        self.ID = ID
+        self.X = X
+        self.Y = Y
+        self.DTR = DTR
+        self.STATE = "IDLE"
+        self.END_TIME = None
+        self.ES = None
+
+
+class ES:
+    def __init__(
+        self,
+        ID,
+        VM_NB,
+        VM_CP,
+    ):
+        self.ID = ID
+        self.VM_NB = VM_NB
+        self.VM_CP = VM_CP
+
+
+class Task:
+    def __init__(
+        self,
+        ID,
+        LENGTH,
+        FILE_SIZE,
+        TYPE,
+        X,
+        Y,
+    ):
+        self.ID = ID
+        self.LENGTH = LENGTH
+        self.FILE_SIZE = FILE_SIZE
+        self.TYPE = TYPE
+        self.X = X
+        self.Y = Y
+        self.COMPLETED = False
+        self.MIGRATION_HISTORY = 0
+        self.COMPUTATION_HISTORY = 0
+        self.MIGRATION_TIME = 0
+
+
+def populate_tasks(data: pd.DataFrame) -> list:
+    """Populates the task list with the tasks from the data file.
+
+    Args:
+        data (pd.DataFrame): dataframe containing the tasks.
+    """
+    tasks = []
+    for i in range(len(data)):
+        task = Task(
+            ID=data["ID"][i],
+            LENGTH=data["LENGTH"][i],
+            FILE_SIZE=data["FILE_SIZE"][i],
+            TYPE=data["TYPE"][i],
+            X=data["X"][i],
+            Y=data["Y"][i],
+        )
+        tasks.append(task)
+
+    return tasks
+
+
+def populate_rsus(data: pd.DataFrame) -> list:
+    """Populates the RSU list with the RSUs from the data file.
+
+    Args:
+        data (pd.DataFrame): dataframe containing the RSUs.
+    """
+    rsus = []
+    for i in range(len(data)):
+        rsu = RSU(
+            ID=data["ID"][i],
+            X=data["X"][i],
+            Y=data["Y"][i],
+            DTR=data["DTR"][i],
+        )
+        rsus.append(rsu)
+
+    return rsus
+
+
+def populate_ess(data: pd.DataFrame) -> list:
+    """Populates the ES list with the ESs from the data file.
+
+    Args:
+        data (pd.DataFrame): dataframe containing the ESs.
+    """
+    ess = []
+    for i in range(len(data)):
+        es = ES(
+            ID=data["ID"][i],
+            VM_NB=data["VM_NB"][i],
+            VM_CP=data["VM_CP"][i],
+        )
+        ess.append(es)
+
+    return ess
+
+
+def get_network(rsu: list, es: list) -> dict:
+    # choose a random number of RSUs M
+    M = np.random.randint(2, len(rsu))
+    # choose a random number of ESs W, with W < M
+    W = np.random.randint(1, M)
+
+    # choose M RSUs from the RSU bank
+    rsu_list = np.random.choice(rsu, M, replace=False)
+    # choose W ESs from the ES bank
+    es_list = np.random.choice(es, W, replace=False)
+
+    # shuffle the RSU list and the ES list
+    np.random.shuffle(rsu_list)
+    np.random.shuffle(es_list)
+
+    # for each es, choose an rsu to connect to
+    for i, es in enumerate(es_list):
+        rsu_list[i].ES = es
+
+    # for each rsu, if ES is None, set it to 'AP'
+    for rsu in rsu_list:
+        if rsu.ES is None:
+            rsu.ES = "AP"
+
+    # shuffle the RSU list again
+    np.random.shuffle(rsu_list)
+
+    return rsu_list
