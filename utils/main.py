12421e32b9a16f2df8d29704c8d09b17914127b4
Lauren Durivault <60437222+laurendudu@users.noreply.github.com>
2023-03-03 23:49:13 +0100

    Network generation (#5)
    * First commit
    
    * Plots
    
    * Graph
    
    * Closest RSU + computations
    
    * Addition of coordinates to the tasks
    
    * Optimization process

--- utils/main.py
+++ utils/main.py
@@ -41,26 +41,58 @@ def data_transfer_time(file_size: int, dtr: int) -> float:
     return file_size / dtr
 
 
-def get_closest_rsu(rsu_list: list, rsu_id: int) -> int:
-    """Returns the index of the closest RSU in the list.
+def get_closest_rsu(rsu_list: dict, x: int, y: int) -> dict:
+    """Returns the index of the closest RSU to a given point.
 
     Args:
-        rsu_list (list): list of RSUs.
-        rsu (int): RSU to compare.
+        rsu_list (dict): list of RSUs.
+        x (int): x coordinate of the point.
+        y (int): y coordinate of the point.
 
     Returns:
         int: index of the closest RSU.
     """
     min_distance = math.inf
     min_index = -1
-    rsu = rsu_list[rsu_id]
     for i in rsu_list.keys():
-        if i != rsu_id:
-            distance = math.sqrt(
-                (rsu_list[i]["RSU_X"] - rsu["RSU_X"]) ** 2
-                + (rsu_list[i]["RSU_Y"] - rsu["RSU_Y"]) ** 2
-            )
-            if distance < min_distance:
-                min_distance = distance
-                min_index = i
+        distance = math.sqrt(
+            (rsu_list[i]["RSU_X"] - x) ** 2 + (rsu_list[i]["RSU_Y"] - y) ** 2
+        )
+        if distance < min_distance and distance != 0:
+            min_distance = distance
+            min_index = i
     return min_index
+
+
+def is_server_free(server: dict) -> bool:
+    """Checks if a server is free.
+
+    Args:
+        server (dict): server to check.
+
+    Returns:
+        bool: True if the server is free, False otherwise.
+    """
+    return server["RSU_STATE"] == "IDLE"
+
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+
+def compatible(rsu, task) -> bool:
+    """Checks if a task is compatible with a RSU.
+
+    Args:
+        rsu (dict): RSU to check.
+        task (dict): task to check.
+
+    Returns:
+        bool: True if the task is compatible with the RSU, False otherwise.
+    """
+    if rsu["ES_ID"] == "AP" and task["TYPE"] == "DATA TRANSFER":
+        return True
+    elif rsu["ES_ID"] != "AP" and task["TYPE"] == "COMPUTATION":
+        return True
+    else:
+        return False
